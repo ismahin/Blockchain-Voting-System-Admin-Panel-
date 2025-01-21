0d3bc582e5ed8925a5b6d332f3db8551ebb9272c
ismahin <mahinshikder01@gmail.com>
2025-01-21 17:07:24 +0600

    Add gh-pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/Blockchain-Voting-System-Admin-Panel-/', // 👈 important! 
+  plugins: [react()]
 })
